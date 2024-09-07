61edc31d259bc11ea84488664b60efa829e3fee8
Wesley <wesmith314@gmail.com>
2024-09-07 16:29:38 -0700

    fixes app atyling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Grow from '@mui/material/Grow';
+import Grid from '@mui/material/Grid';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import experiences from './images/memories.png';
+import useStyles from './styles.js';
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant='h2' align='center'>Experiences</Typography>
+        <img className={classes.image} src={experiences} alt='experiences' height='60'></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
