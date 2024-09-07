8ce9668205dcefe6f0b6ef1f5d4748c5a0e2add0
Wesley <wesmith314@gmail.com>
2024-09-06 23:16:28 -0700

    adds posts logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,27 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req,res) => {
-    res.send("This works");
+export const getPosts = async(req,res) => {
+   try {
+     const PostMessages = await PostMessage.find();
+
+     console.log(PostMessages);
+
+     res.status(200).json(PostMessages);
+   } catch (error) {
+    res.staus(404).json({ message: error.message })
+}
 };
 
-export const createPost = (req,res) => {
-    res.send("Post creation");
+export const createPost = async(req,res) => {
+    const post = req.body;
+
+    const newPost = PostMessage(post)
+
+    try {
+        await newPost.save();
+
+        res.staus(201).json(newPost);
+    } catch (error) {
+        res.staus(409).json({ message: error.message })
+    }
 };
