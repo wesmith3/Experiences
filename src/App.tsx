bcad5a073f1681a9f9a7361345823a9a66f87992
Wesley <wesmith314@gmail.com>
2024-12-10 08:38:48 -0800

    adds react router for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './globals.css'
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello World!
-    </h1>
+    <main className='flex h-screen'>
+      <Routes>
+        
+      </Routes>
+    </main>
   )
 }
 
