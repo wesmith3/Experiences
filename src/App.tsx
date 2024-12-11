408e0fb02156e68a5a0716377642bb56ab07f957
Wesley <wesmith314@gmail.com>
2024-12-11 08:38:48 -0800

    sets up forms file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 
 import './globals.css'
+import SigninForm from './_auth/forms/SigninForm'
 
 function App() {
   return (
     <main className='flex h-screen'>
       <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />}/>
         
+        {/* private routes */}
+        <Route index element={<Home />}/>
       </Routes>
     </main>
   )
