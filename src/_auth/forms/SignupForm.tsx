1eab613b15d65df09aff83f4d6f23aa5a1dc54e1
Wesley <wesmith314@gmail.com>
2024-12-21 08:38:48 -0800

    sets up form for sign up

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,39 @@
-import { Button } from "../../components/ui/button"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 
+import { Button } from "../../components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "../../components/ui/input"
+import { useForm } from "react-hook-form"
+ 
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 
 function SignupForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
     <div>
       <Button>Click Me</Button>
