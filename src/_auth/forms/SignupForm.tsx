14a43b6ca1baf1365ea355018c0477e3a6212425
Wesley <wesmith314@gmail.com>
2024-12-22 08:38:48 -0800

    adds basic form design to signup

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,7 @@
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Button } from "../../components/ui/button"
+import { Button } from "../../components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,13 +10,13 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "../../components/ui/input"
-import { useForm } from "react-hook-form"
- 
+} from "@/components/ui/form";
+import { Input } from "../../components/ui/input";
+import { useForm } from "react-hook-form";
+
 const formSchema = z.object({
   username: z.string().min(2).max(50),
-})
+});
 
 function SignupForm() {
   // 1. Define your form.
@@ -25,20 +25,40 @@ function SignupForm() {
     defaultValues: {
       username: "",
     },
-  })
- 
+  });
+
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
 
   return (
     <div>
-      <Button>Click Me</Button>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
-  )
+  );
 }
 
-export default SignupForm
+export default SignupForm;
